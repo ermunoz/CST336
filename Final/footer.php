1bc8314c3cb41a216dce789b720934805d76417d
ermunoz <43104816+ermunoz@users.noreply.github.com>
2018-12-10 14:10:52 -0800

    Update footer.php

--- Final/footer.php
+++ Final/footer.php
@@ -1,5 +1,5 @@
 <hr>
-<footer style="text-align:center; background-color: #202020; color: white">
+<footer style="text-align:center; background-color: #272b30; color: white">
     <strong>Disclaimer:</strong> CST 336 Final Project <br />
     &copy;2018 Eric Munoz
 </footer>
